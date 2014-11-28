b7578aad4573136c4f2aaea74c297ad966b3d3c8
Ana Tighe <anadances@gmail.com>
2014-11-28 15:18:10 -0800

    fix routes

--- app/common/services.js
+++ app/common/services.js
@@ -2,7 +2,7 @@ var shapenoteServices = angular.module('shapenoteServices', ['ngResource']);
 
 shapenoteServices.factory('Song', ['$resource',
   function($resource){
-    return $resource('songs/:phoneId.json', {}, {
+    return $resource(':songdata.json', {}, {
       query: {method:'GET', params:{phoneId:'phones'}, isArray:true}
     });
   }]);
